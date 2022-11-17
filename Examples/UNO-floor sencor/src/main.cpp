bdaec3a579a94f3f4ee23c597ae74311544347b4
onagi37s <suminagiotibi@icloud.com>
2022-11-17 23:06:28 +0900

    完成版

--- Examples/UNO-floor sencor/src/main.cpp
+++ Examples/UNO-floor sencor/src/main.cpp
@@ -1,31 +1,64 @@
-#include <Arduino.h>
 #include <Adafruit_NeoPixel.h>
+#include <Arduino.h>
 
-const int ledPin = A0;
-const int numOfLeds = 4;
+const int ledPin    = 6;
+const int numOfLeds = 3;
+
+int val[3];
 
 const int sensor = A1;
 
 Adafruit_NeoPixel strip =
-    Adafruit_NeoPixel(A0, numOfLeds, NEO_GRB + NEO_KHZ800);
+    Adafruit_NeoPixel(numOfLeds, ledPin, NEO_GRB + NEO_KHZ800);
 
 void setup() {
     // Initialize the NeoPixels
     strip.begin();
     strip.show();
-    
-    
-    pinMode(sensor,INPUT);
-    Serial.begin(9600);
 
-    for (int i = 0; i < numOfLeds; i++) {
-        strip.setPixelColor(i, 255, 255, 255);
-    }
-    strip.setBrightness(50);
-    strip.show();
+    pinMode(sensor, INPUT);
+    Serial.begin(9600);
 }
 
+long color[3] = {strip.Color(255, 0, 0), strip.Color(0, 20, 0),
+                 strip.Color(0, 0, 230)};
+
 void loop() {
-    int i = analogRead(sensor);
-    Serial.println(i);
+    for (int j = 0; j < 3; j++) {
+        for (int i = 0; i < numOfLeds; i++) {
+            strip.setPixelColor(i, color[j]);
+        }
+        strip.setBrightness(40);
+        strip.show();
+
+        delay(5);
+        val[j] = analogRead(sensor);
+        Serial.print(val[j]);
+        Serial.print("\t");
+    }
+
+    if (val[0] < val[1]) {
+        if (val[0] < val[2]) {
+            Serial.print("赤");
+        } else {
+            Serial.print("青");
+        }
+    } else {
+        if (val[1] < val[2]) {
+            Serial.print("緑");
+        } else {
+            Serial.print("青");
+        }
+    }
+
+    // int cutoff = 600;
+    // if(val[0]<=cutoff){
+    //     if(val[1]<=cutoff){
+    //         if(val[2]<=cutoff){
+    //             Serial.print("\tやっぱり白かも");
+    //         }
+    //     }
+    // }
+
+    Serial.println("");
 }

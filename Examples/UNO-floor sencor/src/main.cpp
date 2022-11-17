5c9e1ce935d998fd9cbfa22e65db31a1dbb525fe
onagi37s <suminagiotibi@icloud.com>
2022-11-17 22:20:33 +0900

    動いたよ

--- Examples/UNO-floor sencor/src/main.cpp
+++ Examples/UNO-floor sencor/src/main.cpp
@@ -1,9 +1,13 @@
 #include <Arduino.h>
-
+const int sensor = A1;
 void setup() {
+  pinMode(sensor,INPUT);
+  Serial.begin(9600);
   // put your setup code here, to run once:
 }
 
 void loop() {
+  int i = analogRead(sensor);
+  Serial.println(i);
   // put your main code here, to run repeatedly:
 }

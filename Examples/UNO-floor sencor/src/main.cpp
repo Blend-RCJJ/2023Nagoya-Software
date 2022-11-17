beaffe9ceba2309a45aabbf1c6c602a5527c0ea7
shirokuma89dev <code@shirokuma89.dev>
2022-11-17 22:19:55 +0900

    NeoPixelを追加

--- Examples/UNO-floor sencor/src/main.cpp
+++ Examples/UNO-floor sencor/src/main.cpp
@@ -1,11 +1,24 @@
 #include <Arduino.h>
+#include <Adafruit_NeoPixel.h>
 
+const int ledPin = A0;
+const int numOfLeds = 4;
 
+Adafruit_NeoPixel strip =
+    Adafruit_NeoPixel(A0, numOfLeds, NEO_GRB + NEO_KHZ800);
 
 void setup() {
-  // put your setup code here, to run once:
+    // Initialize the NeoPixels
+    strip.begin();
+    strip.show();
+
+    for (int i = 0; i < numOfLeds; i++) {
+        strip.setPixelColor(i, 255, 255, 255);
+    }
+    strip.setBrightness(50);
+    strip.show();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+    // put your main code here, to run repeatedly:
 }

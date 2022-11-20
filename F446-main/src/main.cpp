6586bcbf5e6b1eecffd881ff63221b6d313798bf
shirokuma89dev <code@shirokuma89.dev>
2022-11-20 22:12:04 +0900

    ピン番号定義

---
+++ F446-main/src/main.cpp
@@ -0,0 +1,24 @@
+#include <Arduino.h>
+
+const int sw1 = PA4;
+const int sw2 =PA5;
+const int toggle = PA6;
+
+const int ledLeft = PA15;
+const int ledRight = PB13;
+const int ledFloor = PB15;
+const int ledUI = PB14;
+const int ledWhite = PB12;
+const int ledTop = PC1;
+
+const int floorSensor = PC0;
+
+const int buzzer = PB6;
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}

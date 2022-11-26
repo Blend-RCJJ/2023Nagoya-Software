523dcaae15a8e2367a777ca7408872a889e7aa0c
shirokuma89dev <code@shirokuma89.dev>
2022-11-26 17:09:14 +0900

    初期化

--- F446-main/src/main.cpp
+++ F446-main/src/main.cpp
@@ -1,24 +1,195 @@
 #include <Arduino.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BNO055.h>
 
-const int sw1 = PA4;
-const int sw2 =PA5;
-const int toggle = PA6;
+/* Set the delay between fresh samples */
+#define BNO055_SAMPLERATE_DELAY_MS (100)
 
-const int ledLeft = PA15;
-const int ledRight = PB13;
-const int ledFloor = PB15;
-const int ledUI = PB14;
-const int ledWhite = PB12;
-const int ledTop = PC1;
+// Check I2C device address and correct line below (by default address is 0x29
+// or 0x28)
+//                                   id, address
+Adafruit_BNO055 bno = Adafruit_BNO055(55, 0x28, &Wire);
 
-const int floorSensor = PC0;
-
-const int buzzer = PB6;
-
-void setup() {
-  // put your setup code here, to run once:
+/**************************************************************************/
+/*
+    Displays some basic information on this sensor from the unified
+    sensor API sensor_t type (see Adafruit_Sensor for more information)
+*/
+/**************************************************************************/
+void displaySensorDetails(void) {
+    sensor_t sensor;
+    bno.getSensor(&sensor);
+    Serial.println("------------------------------------");
+    Serial.print("Sensor:       ");
+    Serial.println(sensor.name);
+    Serial.print("Driver Ver:   ");
+    Serial.println(sensor.version);
+    Serial.print("Unique ID:    ");
+    Serial.println(sensor.sensor_id);
+    Serial.print("Max Value:    ");
+    Serial.print(sensor.max_value);
+    Serial.println(" xxx");
+    Serial.print("Min Value:    ");
+    Serial.print(sensor.min_value);
+    Serial.println(" xxx");
+    Serial.print("Resolution:   ");
+    Serial.print(sensor.resolution);
+    Serial.println(" xxx");
+    Serial.println("------------------------------------");
+    Serial.println("");
+    delay(500);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+/**************************************************************************/
+/*
+    Display some basic info about the sensor status
+*/
+/**************************************************************************/
+void displaySensorStatus(void) {
+    /* Get the system status values (mostly for debugging purposes) */
+    uint8_t system_status, self_test_results, system_error;
+    system_status = self_test_results = system_error = 0;
+    bno.getSystemStatus(&system_status, &self_test_results, &system_error);
+
+    /* Display the results in the Serial Monitor */
+    Serial.println("");
+    Serial.print("System Status: 0x");
+    Serial.println(system_status, HEX);
+    Serial.print("Self Test:     0x");
+    Serial.println(self_test_results, HEX);
+    Serial.print("System Error:  0x");
+    Serial.println(system_error, HEX);
+    Serial.println("");
+    delay(500);
 }
+
+/**************************************************************************/
+/*
+    Display sensor calibration status
+*/
+/**************************************************************************/
+void displayCalStatus(void) {
+    /* Get the four calibration values (0..3) */
+    /* Any sensor data reporting 0 should be ignored, */
+    /* 3 means 'fully calibrated" */
+    uint8_t system, gyro, accel, mag;
+    system = gyro = accel = mag = 0;
+    bno.getCalibration(&system, &gyro, &accel, &mag);
+
+    /* The data should be ignored until the system calibration is > 0 */
+    Serial.print("\t");
+    if (!system) {
+        Serial.print("! ");
+    }
+
+    /* Display the individual values */
+    Serial.print("Sys:");
+    Serial.print(system, DEC);
+    Serial.print(" G:");
+    Serial.print(gyro, DEC);
+    Serial.print(" A:");
+    Serial.print(accel, DEC);
+    Serial.print(" M:");
+    Serial.print(mag, DEC);
+}
+
+/**************************************************************************/
+/*
+    Arduino setup function (automatically called at startup)
+*/
+/**************************************************************************/
+void setup(void) {
+    Serial.setTx(PA9);
+    Serial.setRx(PA10);
+    Serial.begin(115200);
+
+    while (!Serial) delay(10);  // wait for serial port to open!
+
+    Serial.println("Orientation Sensor Test");
+    Serial.println("");
+
+    /* Initialise the sensor */
+    if (!bno.begin()) {
+        /* There was a problem detecting the BNO055 ... check your connections
+         */
+        Serial.print(
+            "Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
+        while (1)
+            ;
+    }
+
+    delay(1000);
+
+    /* Display some basic information on this sensor */
+    displaySensorDetails();
+
+    /* Optional: Display current status */
+    displaySensorStatus();
+
+    bno.setExtCrystalUse(true);
+}
+
+/**************************************************************************/
+/*
+    Arduino loop function, called once 'setup' is complete (your own code
+    should go here)
+*/
+/**************************************************************************/
+void loop(void) {
+    /* Get a new sensor event */
+    sensors_event_t event;
+    bno.getEvent(&event);
+
+    /* Display the floating point data */
+    Serial.print("X: ");
+    Serial.print(event.orientation.x, 4);
+    Serial.print("\tY: ");
+    Serial.print(event.orientation.y, 4);
+    Serial.print("\tZ: ");
+    Serial.print(event.orientation.z, 4);
+
+    /* Optional: Display calibration status */
+    displayCalStatus();
+
+    /* Optional: Display sensor status (debug only) */
+    // displaySensorStatus();
+
+    /* New line for the next sample */
+    Serial.println("");
+
+    /* Wait the specified delay before requesting nex data */
+    delay(BNO055_SAMPLERATE_DELAY_MS);
+}
+
+// #include <Arduino.h>
+// #include <Adafruit_NeoPixel.h>
+
+// Adafruit_NeoPixel stripL = Adafruit_NeoPixel(7, PA15, NEO_GRB + NEO_KHZ800);
+// Adafruit_NeoPixel stripR = Adafruit_NeoPixel(7, PB13, NEO_GRB + NEO_KHZ800);
+
+// void setup() {
+//     stripR.begin();
+//     stripR.setBrightness(40);
+//     stripR.show();
+
+//     stripL.begin();
+//     stripL.setBrightness(40);
+//     stripL.show();
+// }
+
+// void loop() {
+//     for (double i = 1.0; i >= 0.0; i -= 0.01) {
+//         for (int j = 0; j < 7; j++) {
+//             stripL.setPixelColor(j, 0, 200 * i, 255 * i);
+//         }
+//         for (int j = 0; j < 7; j++) {
+//             stripR.setPixelColor(j, 0, 200 * i, 255 * i);
+//         }
+//         stripR.show();
+//         stripL.show();
+
+//         delay(10);
+//     }
+// }

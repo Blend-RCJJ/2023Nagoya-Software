1e1e591d8156539c726c159e80f0556cd5d0697e
shirokuma89dev <code@shirokuma89.dev>
2022-11-26 17:53:01 +0900

    色

--- F446-main/src/main.cpp
+++ F446-main/src/main.cpp
@@ -4,17 +4,15 @@
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BNO055.h>
 
-/* Set the delay between fresh samples */
-#define BNO055_SAMPLERATE_DELAY_MS (2)
+#include <Adafruit_NeoPixel.h>
+
+Adafruit_NeoPixel stripL = Adafruit_NeoPixel(7, PA15, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel stripR = Adafruit_NeoPixel(7, PB13, NEO_GRB + NEO_KHZ800);
 
 Adafruit_BNO055 bno = Adafruit_BNO055(55, 0x28, &Wire);
 
-/**************************************************************************/
-/*
-    Displays some basic information on this sensor from the unified
-    sensor API sensor_t type (see Adafruit_Sensor for more information)
-*/
-/**************************************************************************/
+#define SPEAKER PB6
+
 void displaySensorDetails(void) {
     sensor_t sensor;
     bno.getSensor(&sensor);
@@ -36,7 +34,7 @@ void displaySensorDetails(void) {
     Serial.println(" xxx");
     Serial.println("------------------------------------");
     Serial.println("");
-    delay(500);
+    // delay(500);
 }
 
 /**************************************************************************/
@@ -59,14 +57,9 @@ void displaySensorStatus(void) {
     Serial.print("System Error:  0x");
     Serial.println(system_error, HEX);
     Serial.println("");
-    delay(500);
+    // delay(500);
 }
 
-/**************************************************************************/
-/*
-    Display sensor calibration status
-*/
-/**************************************************************************/
 void displayCalStatus(void) {
     /* Get the four calibration values (0..3) */
     /* Any sensor data reporting 0 should be ignored, */
@@ -92,105 +85,46 @@ void displayCalStatus(void) {
     Serial.print(mag, DEC);
 }
 
-/**************************************************************************/
-/*
-    Arduino setup function (automatically called at startup)
-*/
-/**************************************************************************/
 void setup(void) {
-        Wire.setSDA(PB9);
+    Wire.setSDA(PB9);
     Wire.setSCL(PB8);
     Wire.begin();
+
     Serial.setTx(PA9);
     Serial.setRx(PA10);
     Serial.begin(9600);
 
-    while (!Serial) delay(10);  // wait for serial port to open!
-
     Serial.println("Orientation Sensor Test");
     Serial.println("");
 
-    /* Initialise the sensor */
-    if (!bno.begin()) {
-        /* There was a problem detecting the BNO055 ... check your
-        connections
-         */
-        Serial.print(
-            "Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
-        while (1)
-            ;
-    }
-
-    delay(1000);
-
-    /* Display some basic information on this sensor */
+    bno.begin();
     displaySensorDetails();
-
-    /* Optional: Display current status */
     displaySensorStatus();
-
     bno.setExtCrystalUse(true);
+
+    stripR.begin();
+    stripR.setBrightness(40);
+    stripR.show();
+
+    stripL.begin();
+    stripL.setBrightness(40);
+    stripL.show();
+
+    pinMode(SPEAKER, OUTPUT);
 }
 
-/**************************************************************************/
-/*
-    Arduino loop function, called once 'setup' is complete (your own code
-    should go here)
-*/
-/**************************************************************************/
 void loop(void) {
-    /* Get a new sensor event */
     sensors_event_t event;
     bno.getEvent(&event);
-
-    /* Display the floating point data */
     Serial.print("X: ");
     Serial.print(event.orientation.x, 4);
-    // Serial.print("\tY: ");
-    // Serial.print(event.orientation.y, 4);
-    // Serial.print("\tZ: ");
-    // Serial.print(event.orientation.z, 4);
-
-    /* Optional: Display calibration status */
-    // displayCalStatus();
-
-    /* Optional: Display sensor status (debug only) */
-    // displaySensorStatus();
-
-    /* New line for the next sample */
     Serial.println("");
 
-    /* Wait the specified delay before requesting nex data */
-    delay(BNO055_SAMPLERATE_DELAY_MS);
+    // delay(BNO055_SAMPLERATE_DELAY_MS);
+    for (int j = 0; j < 7; j++) {
+        stripL.setPixelColor(j, stripL.ColorHSV(event.orientation.x * 182, 255, 255));
+        stripR.setPixelColor(j, stripR.ColorHSV(event.orientation.x * 182, 255, 255));
+    }
+    stripR.show();
+    stripL.show();
 }
-
-// #include <Arduino.h>
-// #include <Adafruit_NeoPixel.h>
-
-// Adafruit_NeoPixel stripL = Adafruit_NeoPixel(7, PA15, NEO_GRB + NEO_KHZ800);
-// Adafruit_NeoPixel stripR = Adafruit_NeoPixel(7, PB13, NEO_GRB + NEO_KHZ800);
-
-// void setup() {
-//     stripR.begin();
-//     stripR.setBrightness(40);
-//     stripR.show();
-
-//     stripL.begin();
-//     stripL.setBrightness(40);
-//     stripL.show();
-// }
-
-// void loop() {
-//     for (double i = 1.0; i >= 0.0; i -= 0.01) {
-//         for (int j = 0; j < 7; j++) {
-//             stripL.setPixelColor(j, 0, 200 * i, 255 * i);
-//         }
-//         for (int j = 0; j < 7; j++) {
-//             stripR.setPixelColor(j, 0, 200 * i, 255 * i);
-//         }
-//         stripR.show();
-//         stripL.show();
-
-//         delay(10);
-//     }
-// }

0964c3be3d7530c76cf268f95c208478677372ad
onagi37s <suminagiotibi@icloud.com>
2022-12-15 00:11:27 +0900

    動いた！

--- F446-main/src/sts3032.cpp
+++ F446-main/src/sts3032.cpp
@@ -2,7 +2,7 @@
 
 #define SPEED 50  // 速度(0~100%)
 
-void STS3032::Servo_Speed(int currentspeed) {
+void STS3032::Servo_Speed(unsigned long currentspeed) {
     currentspeed = SPEED * 100;
     st.WriteSpe(1,currentspeed,0);
 }

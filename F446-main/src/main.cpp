257acde68d142bbd7596704345a3c09931ddc4a7
onagi37s <suminagiotibi@icloud.com>
2022-12-14 09:40:48 +0900

    unable

--- F446-main/src/main.cpp
+++ F446-main/src/main.cpp
@@ -5,15 +5,16 @@
 
 HardwareSerial uart1(PA10, PA9);
 HardwareSerial uart4(PA1, PA0);
+HardwareSerial uart5(PD2, PC12);
 
 RTOS_Kit app;
 
 #include "./SCServo/SCServo.h"
 #include "./lib/bno055.h"
 #include "./lib/mlt8530.h"
+#include "./lib/switchUI.h"
 #include "./lib/vl53l0x.h"
 #include "./lib/ws2812b.h"
-#include "./lib/switchUI.h"
 
 Adafruit_NeoPixel stripL   = Adafruit_NeoPixel(7, PA15, NEO_GRB + NEO_KHZ800);
 Adafruit_NeoPixel stripR   = Adafruit_NeoPixel(7, PB13, NEO_GRB + NEO_KHZ800);
@@ -31,6 +32,8 @@ SWITCHUI ui;
 
 #include "./app/sensorApp.h"
 
+#define SPEED 50
+
 void VictimDectationLED(App) {
     while (1) {
         static int count = 0;
@@ -47,27 +50,49 @@ void VictimDectationLED(App) {
     }
 }  // 被災者発見シグナルApp
 
-void mainApp(App) {
-    // uart1.println("LEDApp開始");
-    // app.start(VictimDectationLED);
+void Drive(App) {
+    while (1) {
+        st.WriteSpe(1,SPEED*100,0);
+        app.delay(6000);
+        st.WriteSpe(1, 0, 0);
+        app.delay(1000);
+        st.WriteSpe(1, -SPEED*100, 0);
+        app.delay(6000);
+        st.WriteSpe(1, 0, 0);
+        app.delay(1000);
+    }
+}
 
-    // while (1) {
-    //     app.delay(100);
-    // }
+void mainApp(App) {
+    uart1.println("LEDApp開始");
+    app.start(Drive);
+
+    while (1) {
+        app.delay(100);
+    }
 }
 
 void setup() {
-    led.start(24);
-    led.show();
+    // led.start(24);
+    // led.show();
 
-    led.leftBootLED(7);
-    led.show();
+    // led.leftBootLED(7);
+    // led.show();
 
-    led.rightBootLED(7);
-    led.show();
+    // led.rightBootLED(7);
+    // led.show();
 
-    led.tktk(100000);
-    led.show();
+    // led.tktk(100000);
+    // led.show();
+
+    uart5.begin(1000000);
+    st.pSerial = &uart5;
+    delay(1000);
+
+    st.unLockEprom(1);
+    st.WheelMode(1);
+    st.EnableTorque(1, 1);
+    st.LockEprom(1);
 
     Wire.setSDA(PB9);
     Wire.setSCL(PB8);
@@ -82,6 +107,7 @@ void setup() {
     app.create(mainApp, firstPriority);
     app.create(VictimDectationLED);
     app.create(inputMonitoringApp, firstPriority);
+    app.create(Drive);
 
     app.start(mainApp);
     app.start(inputMonitoringApp);

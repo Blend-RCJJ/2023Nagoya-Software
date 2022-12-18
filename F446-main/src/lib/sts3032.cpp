5058b84a76ab6ed9da62a154956d06b83e8ac221
shirokuma89dev <code@shirokuma89.dev>
2022-12-18 13:52:49 +0900

    ちこん！！

--- F446-main/src/lib/sts3032.cpp
+++ F446-main/src/lib/sts3032.cpp
@@ -30,32 +30,6 @@ void STS3032::directDrive(int id, int percent, int acceleration) {
     sendData = percent * maximumSpeed / 100;
     sendData = constrain(sendData, -maximumSpeed, maximumSpeed);
 
-    if (cameraLeft.isVictimDetected || cameraRight.isVictimDetected) {
-        led.setUIColor(led.yellow);
-        led.setLeftColor(led.yellow);
-        led.setRightColor(led.yellow);
-        led.setTopColor(led.yellow);
-        led.setUIBrightness(255 * (millis() / 100) % 2);
-        led.setRightBrightness(255 * (millis() / 100) % 2);
-        led.setLeftBrightness(255 * (millis() / 100) % 2);
-        led.setTopBrightness(255 * (millis() / 100) % 2);
-        led.show();
-        serialServo.WriteSpe(id + 1, 0, acceleration);
-        return;
-    } else {
-        // app.stop(LEDtktk);
-
-        led.setUIBrightness(255);
-        led.setRightBrightness(255);
-        led.setLeftBrightness(255);
-        led.setTopBrightness(255);
-        led.setUIColor(led.blue);
-        led.setLeftColor(led.blue);
-        led.setRightColor(led.blue);
-        led.setTopColor(led.blue);
-        led.show();
-    }
-
     if (digitalRead(PA5)) {
         serialServo.WriteSpe(id + 1, sendData, acceleration);
     } else {

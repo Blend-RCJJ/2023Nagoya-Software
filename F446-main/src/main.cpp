2cd074e91235514633cdb1f3e94828abcbbd6e45
shirokuma-89 <code@shirokuma89.dev>
2023-01-12 22:29:23 +0900

    でりしゃす

--- F446-main/src/main.cpp
+++ F446-main/src/main.cpp
@@ -4,6 +4,7 @@
 #include "./lib/RTOS-Kit.h"
 
 HardwareSerial uart1(PA10, PA9);
+HardwareSerial uart2(PA3, PA2);
 HardwareSerial uart4(PA1, PA0);
 HardwareSerial uart5(PD2, PC12);
 HardwareSerial uart6(PC7, PC6);
@@ -16,6 +17,8 @@ RTOS_Kit app;
 #include "./lib/switchUI.h"
 #include "./lib/vl53l0x.h"
 #include "./lib/ws2812b.h"
+#include "./lib/floorSensor.h"
+#include "./lib/unitV.h"
 
 Adafruit_NeoPixel stripL = Adafruit_NeoPixel(7, PA15, NEO_GRB + NEO_KHZ800);
 Adafruit_NeoPixel stripR = Adafruit_NeoPixel(7, PB13, NEO_GRB + NEO_KHZ800);
@@ -31,6 +34,8 @@ WS2812B led(50);
 MLT8530 speaker;
 SWITCHUI ui;
 FLOOR_SENSOR floorSensor;
+UNITV cameraLeft(&uart6);
+UNITV cameraRight(&uart2);
 
 #include "./lib/sts3032.h"
 STS3032 servo(&uart5);
@@ -173,21 +178,15 @@ void isOnBlue(App) {
 }
 
 void mainApp(App) {
+    // app.start(LEDtktk);
     while (1) {
         appMode = 0;
 
         app.start(isOnBlack);
+        app.start(isOnBlue);
         app.start(largeDrive);
-        led.setTopColor(led.red);
-        led.setRightColor(led.red);
-        led.setLeftColor(led.red);
-        led.show();
-        app.delay(20000);
+        app.delay(30000);
         app.start(right);
-        led.setTopColor(led.blue);
-        led.setRightColor(led.blue);
-        led.setLeftColor(led.blue);
-        led.show();
         app.delay(2000);
         app.stop(right);
         // app.start(DriveRight);
@@ -197,18 +196,14 @@ void mainApp(App) {
         appMode = 1;
         app.stop(largeDrive);
         app.start(onlyRight);
-        app.delay(8000);
+        app.delay(10000);
 
         appMode = 2;
         app.stop(onlyRight);
         app.start(onlyLeft);
-        app.delay(8000);
+        app.delay(10000);
         app.stop(onlyLeft);
         app.start(left);
-         led.setTopColor(led.blue);
-        led.setRightColor(led.blue);
-        led.setLeftColor(led.blue);
-        led.show();
         app.delay(2000);
         app.stop(left);
     }
@@ -242,9 +237,13 @@ void setup() {
     uart1.setTx(PA9);
     uart1.begin(115200);
 
-    // uart6.setRx(PC7);
-    // uart6.setTx(PC6);
-    // uart6.begin(115200);
+    uart2.setRx(PA3);
+    uart2.setTx(PA2);
+    uart2.begin(115200);
+
+    uart6.setRx(PC7);
+    uart6.setTx(PC6);
+    uart6.begin(115200);
 
     led.setLeftColor(led.yellow);
     led.setRightColor(led.yellow);
@@ -267,6 +266,7 @@ void setup() {
     app.create(onlyRight);
     app.create(onlyLeft);
     app.create(isOnBlack);
+    app.create(isOnBlue);
     app.create(oooon);
     app.create(right);
     app.create(random);
